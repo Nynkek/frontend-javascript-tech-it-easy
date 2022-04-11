bbb8c724721729e46fa591b91bc94107b227d52a
nynkek <nyn.jaa@gmail.com>
2022-04-11 15:12:38 +0200

    4 is helemaal af

--- opdr4.js
+++ opdr4.js
@@ -9,6 +9,7 @@ tvName(inventory[5]);
 tvName(inventory[1]);
 
 // 4b
+
 function tvPrice(item) {
     let price = "€" + item.price + ",-";
     return price;
@@ -17,11 +18,6 @@ function tvPrice(item) {
 tvPrice(inventory[2]);
 
 // 4c
-// schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc.
-// Als een tv maar één schermgrootte heeft ([32]) wordt de output 32 inch (81 cm).
-// Wanneer een tv vier schermgroottes heeft ([43, 50, 55, 58])
-// wordt de output 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm).
-// Test of jouw functie werkt in alle mogelijke gevallen.
 console.log("4c begint hier")
 
 function inchToCm(inch) {
@@ -54,14 +50,26 @@ console.log("2: " + printDitVoorMe);
 
 
 // opdracht 4d
-//  Zorg ervoor de informatie van één van de tv's zoals het voorbeeld wordt weergegeven op de pagina.
-//  Gebruik hiervoor de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
+
 console.log("hier begin 4d");
 
 let tvResolutionHTML = document.getElementById('tv-weergeven');
 
 function printTv(item) {
-    tvResolutionHTML.innerHTML = `${tvName(item)} <br> ${tvPrice(item)} <br> ${tvResolution(item.availableSizes)}`
+    tvResolutionHTML.innerHTML += `<h3>${tvName(item)}</h3> <p> ${tvPrice(item)} <br> ${tvResolution(item.availableSizes)} </p>`
     return tvResolutionHTML;
 }
+
 printTv(inventory[4]);
+printTv(inventory[1]);
+
+
+// opdracht 4e: tv-generator functie
+
+function showTvs(array) {
+    for (let i = 0; i < array.length; i++) {
+        printTv(array[i]);
+    }
+}
+
+showTvs(inventory);
